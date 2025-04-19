59cdd599fc9426564b8a29fe60497f1e592ba593
FarmanOthman <farmaanothmaan@gmail.com>
2025-04-19 13:43:02 +0300

    Add CreatePostPage route and wrap App component with ToastProvider for notification management

--- Challenge-3/frontend/src/App.jsx
+++ Challenge-3/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import PostListPage from './pages/PostListPage'
 import PostDetailPage from './pages/PostDetailPage'
+import CreatePostPage from './pages/CreatePostPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import NotFoundPage from './pages/NotFoundPage'
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/posts" element={<PostListPage />} />
           <Route path="/posts/:slug" element={<PostDetailPage />} />
+          <Route path="/create-post" element={<CreatePostPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<NotFoundPage />} />
--- Challenge-3/frontend/src/main.jsx
+++ Challenge-3/frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastProvider } from './context/ToastContext'
 import App from './App.jsx'
 import './index.css'
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
